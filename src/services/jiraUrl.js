650fc973faac27d36acb65fbd799c5c6c9ba3aa6
tianhuiying <tianhuiyingde@163.com>
2020-07-28 22:25:09 +0800

    update

---
+++ src/services/jiraUrl.js
@@ -0,0 +1,12 @@
+const baseUrl = 'http://jira.dev.aixuexi.com';
+
+const jiraLogin = baseUrl + "/rest/auth/1/session";
+const getProjects = baseUrl +"/rest/api/2/search";
+const productLine ="TOL"
+const jql = "project+%3D+"+productLine+"+AND+issuetype+%3D+%E9%9C%80%E6%B1%82+ORDER+BY+priority+DESC,+updated+DESC";
+
+export default {
+  jiraLogin,
+  getProjects,
+  jql
+}
