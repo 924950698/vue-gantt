7aa00aedc6930358b5f5e1bbc3af44352e0f3d96
liuxiangdong3@xdf.cn <liuxiangdong3@xdf.cn>
2020-10-04 19:07:17 +0800

    feat: 编辑完成

--- src/components/Gantt.vue
+++ src/components/Gantt.vue
@@ -10,62 +10,99 @@
       <gantt-header slot="header"></gantt-header>
     </gantt-elastic>
     <div class="q-mt-md" />
-    <el-button type="text" @click="dialogFormVisible = true">增加需求 Dialog</el-button>
-      <el-dialog title="新增" :visible.sync="dialogFormVisible">
-        <el-form :model="form">
-           <el-form-item label="id" :label-width="formLabelWidth">
-            <el-input v-model="form.id" autocomplete="off" style="width: 300px"  maxlength=10 show-word-limit></el-input>
-          </el-form-item>
-          <el-form-item label="项目名称" :label-width="formLabelWidth">
-            <el-input v-model="form.label" autocomplete="off" style="width: 300px"  maxlength=10 show-word-limit></el-input>
-          </el-form-item>
-          <el-form-item label="负责人" :label-width="formLabelWidth">
-            <el-input v-model="form.user" autocomplete="off" style="width: 300px"  maxlength=10 show-word-limit></el-input>
-          </el-form-item>
+    <el-button type="text" @click="dialogFormVisible = true, form={}, modalTitle='新增' ">增加需求 Dialog</el-button>
+    <el-dialog :title="modalTitle" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="id" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.id"
+            autocomplete="off"
+            style="width: 300px"
+            maxlength="10"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="项目名称" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.label"
+            autocomplete="off"
+            style="width: 300px"
+            maxlength="10"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="负责人" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.user"
+            autocomplete="off"
+            style="width: 300px"
+            maxlength="10"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
 
-          <el-form-item label="开始时间" :label-width="formLabelWidth">
-             <div class="block">
-              <el-date-picker
-                v-model="form.start"
-                type="date"
-                value-format="yyyy-MM-dd HH-mm-ss"
-                placeholder="选择日期">
-              </el-date-picker>
-            </div>
-          </el-form-item>
+        <el-form-item label="开始时间" :label-width="formLabelWidth">
+          <div class="block">
+            <el-date-picker
+              v-model="form.start"
+              type="date"
+              value-format="yyyy-MM-dd HH-mm-ss"
+              placeholder="选择日期"
+            >
+            </el-date-picker>
+          </div>
+        </el-form-item>
 
-          <el-form-item label="结束时间" :label-width="formLabelWidth">
-             <div class="block">
-              <el-date-picker
-                v-model="form.endDate"
-                type="date"
-                value-format="yyyy-MM-dd HH-mm-ss"
-                placeholder="选择日期">
-              </el-date-picker>
-            </div>
-          </el-form-item>
+        <el-form-item label="结束时间" :label-width="formLabelWidth">
+          <div class="block">
+            <el-date-picker
+              v-model="form.endDate"
+              type="date"
+              value-format="yyyy-MM-dd HH-mm-ss"
+              placeholder="选择日期"
+            >
+            </el-date-picker>
+          </div>
+        </el-form-item>
 
-          <el-form-item label="项目类型" :label-width="formLabelWidth">
-            <el-input v-model="form.proType" autocomplete="off" style="width: 300px"  maxlength=10 show-word-limit></el-input>
-          </el-form-item>
-          <el-form-item label="进度" :label-width="formLabelWidth">
-            <el-input v-model="form.percent" autocomplete="off" style="width: 300px"  maxlength=10 show-word-limit></el-input>
-          </el-form-item>
-          <el-form-item label="风险" :label-width="formLabelWidth">
-            <el-input v-model="form.risk" autocomplete="off" style="width: 300px"  maxlength=10 show-word-limit></el-input>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="add">确 定</el-button>
-        </div>
-      </el-dialog>
-
+        <el-form-item label="项目类型" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.proType"
+            autocomplete="off"
+            style="width: 300px"
+            maxlength="10"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="进度" :label-width="formLabelWidth">
+          <el-input
+            v-model="form.percent"
+            autocomplete="off"
+            style="width: 300px"
+            maxlength="10"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="风险" :label-width="formLabelWidth">
+          <el-input
+            type="textarea"
+            v-model="form.risk"
+            autocomplete="off"
+            style="width: 300px"
+            maxlength="100"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="add">确 定</el-button>
+      </div>
+    </el-dialog>
   </q-page>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 import GanttElastic from "gantt-elastic";
@@ -95,148 +132,13 @@ function getDate(hours) {
 const jiraLink='http://jira.dev.aixuexi.com/browse/';
 const link ='';
 
-let tasks = [
-   
-];
+let tasks = [];
 
-let options = {
-  taskMapping: {
-    progress: "percent",
-  },
-  maxRows: 100,
-  maxHeight: 500,
-  title: {
-    label: "Your project title as html (link or whatever...)",
-    html: false,
-  },
-  row: {
-    height: 24,
-  },
-  calendar: {
-    hour: {
-      display: true,
-    },
-  },
-  chart: {
-    progress: {
-      bar: false,
-    },
-    expander: {
-      display: true,
-    },
-  },
-  taskList: {
-    expander: {
-      straight: false,
-    },
-    columns: [
-      {
-        id: 1,
-        label: "ID",
-        value: "id",
-        width: 40,
-      },
-      {
-        id: 2,
-        label: "项目",
-        value: "label",
-        width: 130,
-        expander: true,
-        html: true,
-        // events: {
-        //   click({ data, column }) {
-        //     alert("description clicked!\n" + data.label);
-        //   }
-        // }
-      },
-      // {
-      //   id: 3,
-      //   label: "优先级",
-      //   value: "level",
-      //   width: 130,
-      //   html: true,
-      // },
-      {
-        id: 4,
-        label: "负责人",
-        value: "user",
-        width: 130,
-        html: true,
-      },
-      {
-        id: 5,
-        label: "开始时间",
-        value: (task) => task.start ? dayjs(task.start).format("YYYY-MM-DD") : "暂无数据",
-        width: 78,
-      },
-      {
-        id: 6,
-        label: "结束时间",
-        value: (task) => task.endDate ? dayjs(task.endDate).format("YYYY-MM-DD") : "暂无数据",
-        width: 78,
-      },
-      {
-        id: 7,
-        label: "项目类型",
-        value: "proType",
-        width: 68,
-        style: {
-          "task-list-header-label": {
-            "text-align": "center",
-            width: "100%",
-          },
-          "task-list-item-value-container": {
-            "text-align": "center",
-            width: "100%",
-          },
-        },
-      },
-      {
-        id: 8,
-        label: "进度%",
-        value: "progress",
-        width: 55,
-        style: {
-          "task-list-header-label": {
-            "text-align": "center",
-            width: "100%",
-          },
-          "task-list-item-value-container": {
-            "text-align": "center",
-            width: "100%",
-          },
-        },
-      },
-      {
-        id: 9,
-        label: "风险",
-        value: "risk",
-        width: 120,
-        style: {
-          "task-list-header-label": {
-            "text-align": "center",
-            width: "100%",
-            color: "red",
-          },
-        },
-      },
-    ],
-  },
-  locale: {
-    name: "en",
-    Now: "Now",
-    "X-Scale": "Zoom-X",
-    "Y-Scale": "Zoom-Y",
-    "Task list width": "Task list",
-    "Before/After": "Expand",
-    "Display task list": "Task list",
-  },
-};
 let actionsType = new Map([
     [1, '产品需求'],
     [2, '技术驱动'],
     [null, '—'],
-]); 
+]);
 
 let actionsPriority = new Map([
     [1, 'P0'],
@@ -244,20 +146,167 @@ let actionsPriority = new Map([
     [3, 'P2'],
     [4, 'P3'],
     [null, '—'],
-]); 
+]);
 
-export default {
+var vm = {
   name: "Gantt",
   components: {
     GanttElastic,
     GanttHeader,
+
   },
   data() {
+    var self = this;
     return {
+      modalTitle: '新增',
       link,
       jiraLink,
       tasks,
-      options,
+      options: {
+    taskMapping: {
+      progress: "percent",
+    },
+    maxRows: 100,
+    maxHeight: 500,
+    title: {
+      label: "Your project title as html (link or whatever...)",
+      html: false,
+    },
+    row: {
+      height: 24,
+    },
+    calendar: {
+      hour: {
+        display: true,
+      },
+    },
+    chart: {
+      progress: {
+        bar: false,
+      },
+      expander: {
+        display: true,
+      },
+    },
+    taskList: {
+      expander: {
+        straight: false,
+      },
+      columns: [
+        {
+          id: 0,
+          label: "操作",
+          value: "edit",
+          width: 60,
+          html: true,
+          events: {
+            click({ data, column }) {
+              self.handleToEdit(data);
+            }
+          }
+        },
+        {
+          id: 1,
+          label: "ID",
+          value: "id",
+          width: 40,
+        },
+        {
+          id: 2,
+          label: "项目",
+          value: "label",
+          width: 130,
+          expander: true,
+          html: true,
+          // events: {
+          //   click({ data, column }) {
+          //     alert("description clicked!\n" + data.label);
+          //   }
+          // }
+        },
+        // {
+        //   id: 3,
+        //   label: "优先级",
+        //   value: "level",
+        //   width: 130,
+        //   html: true,
+        // },
+        {
+          id: 4,
+          label: "负责人",
+          value: "user",
+          width: 130,
+          html: true,
+        },
+        {
+          id: 5,
+          label: "开始时间",
+          value: (task) => task.start ? dayjs(task.start).format("YYYY-MM-DD") : "暂无数据",
+          width: 78,
+        },
+        {
+          id: 6,
+          label: "结束时间",
+          value: (task) => task.endDate ? dayjs(task.endDate).format("YYYY-MM-DD") : "暂无数据",
+          width: 78,
+        },
+        {
+          id: 7,
+          label: "项目类型",
+          value: "proType",
+          width: 68,
+          style: {
+            "task-list-header-label": {
+              "text-align": "center",
+              width: "100%",
+            },
+            "task-list-item-value-container": {
+              "text-align": "center",
+              width: "100%",
+            },
+          },
+        },
+        {
+          id: 8,
+          label: "进度%",
+          value: "progress",
+          width: 55,
+          style: {
+            "task-list-header-label": {
+              "text-align": "center",
+              width: "100%",
+            },
+            "task-list-item-value-container": {
+              "text-align": "center",
+              width: "100%",
+            },
+          },
+        },
+        {
+          id: 9,
+          label: "风险",
+          value: "risk",
+          width: 120,
+          style: {
+            "task-list-header-label": {
+              "text-align": "center",
+              width: "100%",
+              color: "red",
+            },
+          },
+        },
+      ],
+    },
+    locale: {
+      name: "en",
+      Now: "Now",
+      "X-Scale": "Zoom-X",
+      "Y-Scale": "Zoom-Y",
+      "Task list width": "Task list",
+      "Before/After": "Expand",
+      "Display task list": "Task list",
+    },
+  },
       dynamicStyle: {},
       lastId: 16,
       // 新增弹窗属性
@@ -272,7 +321,7 @@ export default {
         duration: 14 * 24 * 60 * 60 * 1000, // 时常
         percent: 10, //完成度
         type: '',
-        proType:'', 
+        proType:'',
         risk: '', //风险
       },
       value1: '',
@@ -280,19 +329,57 @@ export default {
   },
 
   mounted() {
+    var _this = this
     this.queyGanttList();
   },
 
   methods: {
 
+    handleToEdit(rows) {
+      this.modalTitle = '编辑';
+      this.form = {
+        id: rows.id,
+        label: rows.label, // 项目名称
+        user: rows.user,  // 负责人
+        start: this.getDate('yyyy-mm-dd', rows.start),//rows.start, // 开始时间
+        endDate: this.getDate('yyyy-mm-dd', rows.endDate), //结束时间
+        percent: rows.percent, //完成度
+        type: rows.type,
+        proType: rows.proType,
+        risk: rows.risk, //风险
+      };
+      this.dialogFormVisible = true;
+      console.log( this,'--rows--');
+    },
+
+
+    //转换年月日方法
+  getDate(format,str){
+    var oDate = new Date(str),
+    oYear = oDate.getFullYear(),
+    oMonth = oDate.getMonth()+1,
+    oDay = oDate.getDate(),
+    oHour = oDate.getHours(),
+    oMin = oDate.getMinutes(),
+    oSec = oDate.getSeconds();
+    return oYear +'-'+ this.getzf(oMonth) +'-'+ this.getzf(oDay) +' '+ this.getzf(oHour) +':'+ this.getzf(oMin) +':'+this.getzf(oSec);//最后拼接时间
+  },
+
+  //补0操作  
+  getzf(num){  
+    if(parseInt(num) < 10){  
+        num = '0'+num;  
+    }  
+    return num;  
+  },
+
     add() {
-      const _this = this;
       this.dialogFormVisible = false;
       const params = this.form;
       this.axios.post(services.add, params).then((res) => {
         if(res.data) {
-          _this.queyGanttList();
-        } 
+          this.queyGanttList();
+        }
       })
     },
 
@@ -306,7 +393,7 @@ export default {
                 item.start = getDate(24 * getStartDate(item.start));
             }
             if(item.endDate) {
-                item.endDate = getDate(24 * getStartDate(item.endDate)); 
+                item.endDate = getDate(24 * getStartDate(item.endDate));
             }
             if(item.endDate && item.start ) {
               item.duration = item.endDate - item.start;
@@ -324,13 +411,14 @@ export default {
                 };
             }
             item.type = "milestone";
+            item.edit = ` <a style="color:blue; cursor:pointer;"> 编辑 </a>`;
             this.tasks.push(item);
           });
 
         }
       });
-    },  
-    
+    },
+
     addTask() {
         // this.tasks.push({
         //   id: this.lastId++,
@@ -344,7 +432,7 @@ export default {
         //   type: "project",
         // });
     },
-    
+
     tasksUpdate(tasks) {
       this.tasks = tasks;
     },
@@ -359,4 +447,6 @@ export default {
 
   },
 };
+
+export default vm;
 </script>
